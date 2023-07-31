b0038e04a4d206b1a09ce300d493ba74e7e46ce2
Florian Liebig <hello@florian-liebig.de>
2023-07-31 09:59:49 +0200

    middleware process.env error

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className="[color-scheme:dark]">
+      <body className="bg-gray-1100 overflow-y-scroll bg-[url('/grid.svg')] pb-36">
+        {children}
+      </body>
+    </html>
+  );
+}
